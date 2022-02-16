328c35d2e80ca20e6d9694b412596770fd7b5fe6
bosungpark <qkrqhtjd0806@naver.com>
2022-02-16 09:35:27 +0900

    TEST: little change in seed

--- backend/testproject/management/commands/seed_photos.py
+++ backend/testproject/management/commands/seed_photos.py
@@ -7,17 +7,18 @@ from testproject import models as post_models
 import random
 import numpy as np
 
-
-
 class Command(BaseCommand):
     
     help = "This command creates posts"
 
+    def __init__(self):
+        self.random_cnt = 0
+        self.ones = []
+        self.zeros = []
+
     def init_data(self):
         print("init starts")
         # '0' as a reward from each ad
-        ones = [] # '1' as a reward from each ad
-        zeros = []
         # 데이터 랜덤추출 
         seed = 12
         random.seed(seed)
@@ -34,28 +35,28 @@ class Command(BaseCommand):
         rounds  = 100
         clicks = []
 
+        self.ones = np.full(rounds, 0)
+        self.zeros = np.full(rounds, 0)
+
         # 알파, 베타값의 배열 초기화 
         for _ in range(rounds):
             for i, a in enumerate(arms):  # 랜덤추출 
-                value = np.random.binomial(1, a, rounds) # arms의 확률대로 초기화:  0 or 1 
-                if value == 1: 
+                value = np.random.binomial(1, a, rounds) # arms의 확률대로 초기화:  0 or 1
+                print(f" idx : {i}")
+                if value[i] == 1: 
                     self.ones[i] += 1
                 else:
-                    self.zeros[i]+= 1
-        
-        post_list = post_models.testData.objects.all()
-
-        for i, post in enumerate(post_list):
-            post.views_cnt =  ones[i]
-            print(f"views_cnt : {post.views_cnt}")
-            post.impressions_cnt=  zeros[i]
-            post.save()
+                    self.zeros[i] += 1
         print("init ends")
         
     def add_arguments(self, parser):
         parser.add_argument("--number", default=2, type=int, help="how many posts do you want")
 
     def handle(self, *args, **options):
+
+        if self.random_cnt == 0:
+            self.init_data()
+
         number = options.get("number")
         seeder = Seed.seeder()
         all_user = user_models.User.objects.all()
@@ -65,15 +66,15 @@ class Command(BaseCommand):
         seeder.add_entity(post_models.testData, number, {
             "user": lambda x: random.choice(all_user),
             "image": lambda x:  f"images/insta{random.choice(random_idx)}.jpg",
-
-            "views_cnt": lambda x: random.randint(1,10000),
-            "impressions_cnt": lambda x: random.randint(100000,10000000),
+            "views_cnt": lambda x: self.ones[self.random_cnt],
+            "impressions_cnt": lambda x: self.zeros[self.random_cnt],
             "text_length": lambda x: random.randint(1,10000),
             "image_cnt": lambda x: random.randint(1,10000),
             "like": lambda x:  random.randint(1,10000),
             "importance":lambda x: random.randint(1,10000),
         })
         created_room = seeder.execute()
-        created_clean = flatten(list(created_room.values()))
 
-        self.stdout.write(self.style.SUCCESS(f"{number} of lists Created"))
+        self.random_cnt += 1
+
+        created_clean = flatten(list(created_room.values()))
