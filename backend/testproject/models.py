2f386adb1628d280414d76aacbe06d6208463754
bosungpark <qkrqhtjd0806@naver.com>
2022-02-16 16:45:29 +0900

    FIX: positive integer field

--- backend/testproject/models.py
+++ backend/testproject/models.py
@@ -13,14 +13,14 @@ class testData(models.Model):
     )
 
     #알고리즘에 영향을 주는 요소
-    views_cnt=models.IntegerField(null=True,blank=True)#조회수  
-    impressions_cnt=models.IntegerField(default=1)#노출수
-    text_length=models.IntegerField(null=True,blank=True)#글자 길이
-    image_cnt=models.IntegerField(null=True,blank=True,default=1)#이미지 갯수
+    views_cnt=models.PositiveIntegerField(null=True,blank=True)#조회수  
+    impressions_cnt=models.PositiveIntegerField(default=1)#노출수
+    text_length=models.PositiveIntegerField(null=True,blank=True)#글자 길이
+    image_cnt=models.PositiveIntegerField(null=True,blank=True,default=1)#이미지 갯수
 
     #알고리즘에 영향을 주는 요소이며 개인적인 레코드가 기록되는 요소, 모델을 이후 따로 만들어야 할듯.
     residence_time=models.FloatField(null=True,blank=True,default=0)#사람들의 누적 체류시간(ms), 평균적인 체류시간은 조회수로 나누면 될 듯.
-    like=models.IntegerField(null=True,blank=True)#좋아요
+    like=models.IPositiveIntegerField(null=True,blank=True)#좋아요
 
     importance=models.FloatField(null=True,blank=True,default=0)#중요도
 
