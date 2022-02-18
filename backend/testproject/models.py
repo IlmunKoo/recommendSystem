6e55f566d7dd3ba519aeab8d8f7d19f6a9211647
bosungpark <qkrqhtjd0806@naver.com>
2022-02-18 16:23:16 +0900

    fix user recommend

--- backend/testproject/models.py
+++ backend/testproject/models.py
@@ -13,10 +13,10 @@ class TestData(models.Model):
     )
 
     #알고리즘에 영향을 주는 요소
-    views_cnt=models.PositiveIntegerField(null=True,blank=True)#조회수  
-    exposure=models.PositiveIntegerField(null=True,blank=True,default=0)#총 노출수
+    views_cnt=models.PositiveIntegerField(null=True,blank=True,default=1)#조회수  
+    exposure=models.PositiveIntegerField(null=True,blank=True,default=1)#총 노출수
     impressions_cnt=models.IntegerField(default=1)#노출되었지만 클릭 안된수. 현재 테스트 케이스의 문제로 음수 허용, 이후 수정 필요.
-    text_length=models.PositiveIntegerField(null=True,blank=True)#글자 길이
+    text_length=models.PositiveIntegerField(null=True,blank=True,default=1)#글자 길이
     image_cnt=models.PositiveIntegerField(null=True,blank=True,default=1)#이미지 갯수
 
     #알고리즘에 영향을 주는 요소이며 개인적인 레코드가 기록되는 요소, 모델을 이후 따로 만들어야 할듯.
@@ -28,7 +28,11 @@ class TestData(models.Model):
 
 class Comment(models.Model):
     created_date=models.DateTimeField(auto_now_add=True, null=True)
-
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #글 쓴 유저
+    post = models.ForeignKey(TestData, null=True, blank=True, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)# like 한 유저
     post = models.ForeignKey(TestData, null=True, blank=True, on_delete=models.CASCADE)
